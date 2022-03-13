b86afac78c935d57da1040e2c4835e6363b8bbdf
David Matalik <david.matalik@posteo.de>
2022-03-13 08:57:59 +0100

    Add general text for startpage

--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -25,6 +25,40 @@ function StartPage({
           />
         </section>
       </div>
+      {!user && (
+        <section className='start-page-no-user'>
+          <h2>Das ist GoodJobs</h2>
+          <p>
+            GoodJobs ist Deutschlands größte Plattform für nachhaltige und
+            soziale Jobs. Gemeinsam mit unseren Partnerorganisationen und
+            unserer Community möchten wir die Arbeitswelt verändern – weg von
+            reinem Profitstreben, hin zu mehr Leidenschaft für das, was zählt:
+            Sinnvolles Handeln für den Erhalt unseres Planeten und die Chance
+            auf ein gutes Leben für alle!
+          </p>
+          <h2>Unsere Vision für die Zukunft</h2>
+          <p>
+            Wir wünschen uns eine Welt, in der alle Menschen mit ihrem GoodJob
+            etwas Gutes bewirken und dabei glücklich und zufrieden sind. Eine
+            Welt, in der alle Arbeitgeber nachhaltige Unternehmensziele und eine
+            wertschätzende Organisationskultur verfolgen.
+          </p>
+          <h2>Unsere Mission</h2>
+          <p>
+            Wir möchten zeigen, dass es diese Jobs wirklich gibt und sie kein
+            Traum bleiben müssen. Denn für fast jeden “herkömmlichen” Job gibt
+            es mittlerweile ein Gegenstück im Themenfeld der Nachhaltigkeit oder
+            im sozialen Bereich. Damit alle nachhaltigen und sozialen
+            Arbeitgeber ihre positive Wirkung maximieren können, bieten wir
+            ihnen eine Plattform und verhelfen ihnen zu den motiviertesten
+            Mitarbeiter*innen. Wir informieren und inspirieren unsere Community
+            regelmäßig mit Inhalten zu Themen wie New Work, Nachhaltigkeit im
+            Job und Zukunft der Arbeit. GoodJobs ist mehr als eine Jobbörse: Wir
+            bieten eine Perspektive für eine bessere Zukunft – für Jobsuchende,
+            Organisationen und uns alle.
+          </p>
+        </section>
+      )}
       {user && (
         <section className='start-page-favorites'>
           <JobResult
